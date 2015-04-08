0a113a60ed10051008d1cf33fd58f878d929f757
shermpay <shermanpay1991@gmail.com>
2015-04-07 23:34:19 -0700

    Improve command line parsing

--- src/Main.hs
+++ src/Main.hs
@@ -4,15 +4,42 @@
 
 import qualified System.Environment as Env
 import qualified System.IO as IO
+import System.Console.GetOpt
+import Data.Maybe ( fromMaybe )
 
 import qualified Tiger.Parser as Parser
+    
+version = "0.1"
+data Flag = Parse IO.FilePath
+          | Version
+          | Help
+          deriving Show
+
+options :: [OptDescr Flag]
+options =
+    [ Option ['p'] ["parse"] (ReqArg Parse "FILE") "parse FILE"
+    , Option ['h'] ["help"]  (NoArg Help) "Prints this usage string" ]
     
 parseFile :: String -> IO Parser.Prog
 parseFile fileName = do
     sourceCode <- IO.readFile fileName
     return $ Parser.parseProg fileName sourceCode
 
-main :: IO Parser.Prog
+handleOpt :: Flag -> IO ()
+handleOpt (Parse f) = do
+  prog <- parseFile f
+  putStrLn $ show prog
+  return ()
+
+handleOpt Help = usage
+         
+usage :: IO ()
+usage = putStr $ usageInfo "Usage: tiger [options] [args]" options
+
+main :: IO ()
 main = do
   args <- Env.getArgs
-  parseFile (args !! 0)
+  case getOpt Permute options args of
+    (optArgs, _, []) -> handleOpt $ head optArgs
+    (_, _, err) -> do { putStr $ head err; usage }
+                   
