5e0eb684a8e1dfc9932ede0ef05f043ba6aa14bd
shermpay <shermanpay1991@gmail.com>
2015-07-12 11:36:29 -0700

    Separated AST from Parser

--- src/Main.hs
+++ src/Main.hs
@@ -5,34 +5,38 @@
 import qualified System.Environment as Env
 import qualified System.IO as IO
 import System.Console.GetOpt
-import Data.Maybe ( fromMaybe )
+import System.Exit
 
+import qualified Tiger.AST as AST
 import qualified Tiger.Parser as Parser
     
-version = "0.1"
+-- | Command line flags
 data Flag = Parse IO.FilePath
           | Version
           | Help
           deriving Show
 
+-- | Command line options
 options :: [OptDescr Flag]
 options =
     [ Option ['p'] ["parse"] (ReqArg Parse "FILE") "parse FILE"
     , Option ['h'] ["help"]  (NoArg Help) "Prints this usage string" ]
     
-parseFile :: String -> IO Parser.Prog
+-- | Parses a file given a String representing the filename
+parseFile :: String -> IO AST.Prog
 parseFile fileName = do
     sourceCode <- IO.readFile fileName
     return $ Parser.parseProg fileName sourceCode
 
+-- | Handle various command line options
 handleOpt :: Flag -> IO ()
 handleOpt (Parse f) = do
   prog <- parseFile f
   putStrLn $ show prog
   return ()
-
 handleOpt Help = usage
          
+-- | Print usage string
 usage :: IO ()
 usage = putStr $ usageInfo "Usage: tiger [options] [args]" options
 
@@ -40,7 +44,9 @@ main :: IO ()
 main = do
   args <- Env.getArgs
   case getOpt Permute options args of
-    (optArgs, _, []) -> case optArgs of 
-                          arg:_ -> handleOpt $ arg
-                          [] -> usage
-    (_, _, err) -> do { putStr $ head err; usage }
+    (optArgs, _, []) -> do  
+         case optArgs of 
+           arg:_ -> handleOpt $ arg
+           [] -> usage
+         System.Exit.exitSuccess
+    (_, _, err) -> do { putStr $ head err; usage; System.Exit.exitFailure }
--- src/Tiger/Parser.hs
+++ src/Tiger/Parser.hs
@@ -1,20 +1,18 @@
-module Tiger.Parser ( parseProg
-                    , Prog 
-                    , Expr) where
+module Tiger.Parser ( parseProg ) where
   
-import qualified Data.List as List
 import qualified Data.Map as Map
 import Data.Functor.Identity (Identity)
 import Control.Monad
-import Control.Applicative ((<$>))
 import Text.Parsec
 import Text.Parsec.String
 import Text.Parsec.Expr
-import Text.Parsec.Pos
 -- import Text.ParserCombinators.Parsec
 import qualified Text.Parsec.Token as Tok
+    
+import Tiger.AST
 
 {- Token Definitions -}
+def :: Tok.GenLanguageDef String () Identity
 def = Tok.LanguageDef { Tok.commentStart = "/*"
                       , Tok.commentEnd = "*/"
                       , Tok.commentLine = ""
@@ -57,29 +55,9 @@ lexer@Tok.TokenParser { Tok.identifier = identifier
              
 lang = "tiger"
        
-type Id = String
-type Comment = String
-    
-------------------
--- Declarations --
-------------------
-data Decl = VarDecl { varName :: Id
-                    , varType :: Maybe Type
-                    , varExpr :: Expr}
-          | FunctionDecl FunctionType Expr
-          | TypeDecl Type Type
-          deriving Show
-
 -----------------------
 -- Type Declarations --
 -----------------------
-type FieldType = (Id, Type)
-type RecordType = [FieldType]
-data Type = Type Id                 -- Simple Type
-          | RecordType RecordType -- Record Type (Field, Type)
-          | ArrayType Type
-            deriving (Show, Eq)
-
 typeIdParser :: Parser Type
 typeIdParser = liftM Type identifier 
 
@@ -150,10 +128,6 @@ readVarDecl input =
 -------------------------------------
 -- Function/Procedure Declarations --
 -------------------------------------
-data FunctionType = FuncType Id RecordType Type
-                  | ProcType Id RecordType
-                    deriving (Show, Eq)
-
 functionDeclParser :: Parser Decl
 functionDeclParser = do
   reserved "function"
@@ -223,20 +197,6 @@ readLValue input =
 -----------------
 -- Expressions --
 -----------------
-data Op = Add
-        | Sub
-        | Mult
-        | Div
-        | Eq
-        | NE
-        | Less
-        | LessEq
-        | Greater
-        | GreaterEq
-        | And
-        | Or
-        deriving (Show, Ord, Eq)
-
 operatorMap :: Map.Map Op String
 operatorMap = Map.fromList [ (Add, "+")
                            , (Sub, "-")
@@ -252,57 +212,6 @@ operatorMap = Map.fromList [ (Add, "+")
                            , (Or, "|")
                            ]
 
-data Expr = IdExpr SourcePos Id
-          | FieldDeref SourcePos Expr Expr -- (Record, Field)
-          | ArraySub SourcePos Expr Expr
-          | Nil SourcePos 
-          | IntConst SourcePos Integer
-          | StringLit SourcePos String
-          | SeqExpr SourcePos [Expr]
-          | Neg SourcePos Expr
-          | Call SourcePos Expr [Expr]
-          | InfixOp SourcePos Op Expr Expr
-          | NewArr {arrayType :: Type,  arraySize :: Expr, arrayInit ::  Expr
-                   , arrayPos :: SourcePos }
-          | NewRec SourcePos Type [FieldInit]
-          | Assign SourcePos Expr Expr
-          | If { ifTest :: Expr, thenExpr :: Expr,  elseExpr :: Maybe Expr
-               , ifPos :: SourcePos }
-          | While{ whileTest :: Expr, whileBody :: Expr, whilePos :: SourcePos }
-          | For { forVarName :: Id, forVarExpr :: Expr, toExpr :: Expr, doExpr :: Expr 
-                , forPos :: SourcePos }
-          | Break SourcePos
-          | Let { letDecls :: [Decl], letBody ::  [Expr], letPos :: SourcePos }
-          deriving Show
-                
-showListData :: [Expr] -> String
-showListData lst =
-    '[' : (concat $ List.intersperse ", " $ map showData lst) ++ "]"
-
-wrapIn :: String -> String -> String -> String
-wrapIn l r str = l ++ str ++ r
-wrapParens :: String -> String
-wrapParens = wrapIn "(" ")"
-
-spacedConcat :: [String] -> String
-spacedConcat = concat . List.intersperse " "
-
-nodeStr :: [String] -> String
-nodeStr = wrapParens . spacedConcat
-
-instance ASTNode Expr where
-    showData (Nil _) = wrapParens "Nil"
-    showData (IntConst _ x) = nodeStr ["IntConst", (show x)]
-    showData (StringLit _ x) = nodeStr ["StringLit", (show x)]
-    showData (SeqExpr _ e) = nodeStr ["SeqExpr", (showListData e)]
-    showData (Call _ f a) = nodeStr ["Call", (show f), (showListData a)]
-    showData (InfixOp _ op e1 e2) = nodeStr
-                                  ["InfixOp", (show op), (showData e1), (showData e2)]
-    showData (NewArr {arrayType=t, arraySize=s, arrayInit=i}) =
-        nodeStr ["NewArr", (show t), (showData s), (showData i)]
-    showData e = wrapParens $ show e
-                   
-type FieldInit = (Id, Expr)
 
 idAndExprParser :: Parser Expr
 idAndExprParser = (try arraySubParser 
@@ -456,8 +365,6 @@ readExpr input =
 --------------
 -- Exported --
 --------------
-type Prog = Expr
-
 class (Show a) => ASTNode a where
     showData :: a -> String
     -- sourcePos :: a -> SourcePos
