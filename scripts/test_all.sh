8f5b8542ef6c459782bd5717b799ef30726a0a9d
shermpay <shermanpay1991@gmail.com>
2015-07-13 20:50:21 -0700

    Adding logging for test

--- scripts/test_all.sh
+++ scripts/test_all.sh
@@ -1,48 +1,68 @@
 #!/bin/bash
 
-EXECUTABLE=$HOME/Dropbox/Projects/HaskellTiger/.cabal-sandbox/bin/HaskellTiger
+PROJ_DIR=$HOME/Dropbox/Projects/HaskellTiger
+EXECUTABLE=$PROJ_DIR/.cabal-sandbox/bin/HaskellTiger
+LOG_DIR=$PROJ_DIR/.cabal-sandbox/logs
 TIGER_FLAGS=( '-p' '--parse' '-h' '--help' )
 flag=$1
 
 num_passed=0
 test_files=( ${@:2} )
 
+usage() {
+    echo 'usage: ./test_all.sh [TIGER_FLAG] [INPUT_FILE]'
+    echo -e 'Possible Tiger Flags:'
+    echo -e '  '${TIGER_FLAGS[@]}
+}
+if [[ $flag == '' ]] ; then
+    usage
+    exit 1
+fi
+
+print() {
+    echo -e $1 | tee -a $logfile
+}
+
 for f in $TIGER_FLAGS; do
     if [[ $flag == $f ]] ; then
-        echo 'Testing '$flag' flag'
+        print 'Testing '$flag' flag'
+        # Create a logging file
+        logfile=$LOG_DIR'/test_'$(date +%s)'.log'
+        touch $logfile
         # Iterate over all the files
         for file in ${test_files[@]}; do
             suffix=${file##*.}
             if [[ $suffix == 'tig' ]] ; then
-                echo -e '\e[33m'"$EXECUTABLE $flag $file"'\e[0m'
-                echo '----------------------------------------------------------------'
+                print '\e[33m'"$EXECUTABLE $flag $file"'\e[0m'
+                print '----------------------------------------------------------------'
                 # Run executable with flag on file
-                $EXECUTABLE $flag $file
+                $EXECUTABLE $flag $file | tee -a $logfile
                 ret=$?
-                echo '----------------------------------------------------------------'
+                print '----------------------------------------------------------------'
                 # Output Pass/Fail
                 if [[ $ret -eq 0 ]] ; then
                     (( num_passed++ ))
-                    echo -e '\e[30;42m''PASSED''\e[0m'
+                    print '\e[30;42m''PASSED''\e[0m'
                 else
-                    echo -e '\e[41m''FAILED''\e[0m'
+                    print '\e[41m''FAILED''\e[0m'
                 fi
-                echo '================================================================'
+                print '================================================================'
             else
-                echo $file' does not have suffix ".tig"'
+                print $file' does not have suffix ".tig"'
             fi
         done
 
         # Output results
         total=${#test_files[@]}
-        echo 'Test Results'
-        echo '------------'
-        echo -e '+ Total:\t'$total
-        echo -e '+ Passed:\t'$num_passed
-        echo -e '+ Failed:\t'$(( $total - $num_passed ))
+        print 'Test Results'
+        print '------------'
+        print '+ Total:\t'$total
+        print '+ Passed:\t'$num_passed
+        print '+ Failed:\t'$(( $total - $num_passed ))
 
         exit 0
     fi
 done
 
+usage
 exit 1
